15f91a9e83da3b8d7890817ddec7c3e623473a2e
azizsalafi2008 <azizsalafi2008@yahoo.com>
2026-08-21 22:33:44 +0300

    Update validation-key.txt.js

--- api/validation-key.txt.js
+++ api/validation-key.txt.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
-  res.status(200).send('477dviz77vzk3csh2gei4xv9dji9hxkwq8dyju9fgqxmfhsc7s1adhqoqxflfdrp');
+  res.status(200).send('361138f95a13b12601fd35b5d8806f5841d81a685bfa210eb03d804ec0687b3ab8b9fe782a4082510c5e68060bcce737759bb621e6027870b29081b20168434b');
 }
