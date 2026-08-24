cf6859a43cc3b58faf55f9f7bc541e46a76a64ef
azizsalafi2008 <azizsalafi2008@yahoo.com>
2026-08-24 19:55:36 +0300

    Delete api/validation-key.txt.js

--- api/validation-key.txt.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
-  res.status(200).send('361138f95a13b12601fd35b5d8806f5841d81a685bfa210eb03d804ec0687b3ab8b9fe782a4082510c5e68060bcce737759bb621e6027870b29081b20168434b');
-}
